92dd4ef52b00c46fbd600fce6cb636d9bb105895
Will <wilitonjr@hotmail.com>
2025-02-12 18:27:55 -0800

    fix logout bug

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using SQLSafeLoginPoc;
+﻿using Microsoft.Owin.Hosting;
+using SQLSafeLoginPoc;
 using System;
 using System.Windows.Forms;
 
@@ -12,9 +13,15 @@ namespace SQLSafe.Login.Poc
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            string baseAddress = "http://localhost:5000/";
+
+            // Start OWIN host
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
         }
     }
 }
